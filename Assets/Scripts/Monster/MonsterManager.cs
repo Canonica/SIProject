27d3d6c7d14b62600a507a76ef19fc631e8cb39e
benoit vidot <b.vidot@rubika-edu.com>
2016-05-09 16:38:16 +0200

    Add Bounce shield

---
+++ Assets/Scripts/Monster/MonsterManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MonsterManager : MonoBehaviour {
+    private static MonsterManager _instance = null;
+
+    public static MonsterManager GetInstance()
+    {
+        return _instance;
+    }
+
+    void Awake()
+    {
+        _instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public List<GameObject> _listOfMonster;
+
+
+    void Start()
+    {
+        GameObject[] _tempMonsters = GameObject.FindGameObjectsWithTag("Enemy");
+        foreach (GameObject parPlayers in _tempMonsters)
+        {
+            _listOfMonster.Add(parPlayers);
+        }
+    }
+
+    public void AddMonster(GameObject parMonster)
+    {
+        _listOfMonster.Add(parMonster);
+    }
+
+    public void RemoveMonster(GameObject parMonster)
+    {
+        _listOfMonster.Remove(parMonster);
+    }
+
+}
---
+++ Assets/Scripts/Shield/ShieldBounce.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+[RequireComponent(typeof(Rigidbody))]
+public class ShieldBounce : MonoBehaviour {
+
+    float m_bumpForce = 10f;
+    float m_bounceCount;
+    float m_maxBounceCount = 3;
+    Rigidbody m_rigidBody;
+
+    float m_currentDistanceMin = Mathf.Infinity;
+    GameObject m_target;
+    
+    void Start()
+    {
+        m_rigidBody = GetComponent<Rigidbody>();
+    }
+
+    void OnTriggerEnter(Collider parCollider)
+    {
+        if(parCollider.gameObject.tag =="Enemy")
+        {
+            m_bounceCount++;
+            Vector3 m_tempEnemyPosition = parCollider.transform.position;
+            Vector3 m_tempPlayerPosition = transform.position;
+            Vector3 m_bumpDirection = m_tempEnemyPosition - m_tempPlayerPosition;
+            m_bumpDirection.Normalize();
+
+            parCollider.transform.DOMove(m_bumpDirection* m_bumpForce, 0.5f);
+
+            MonsterManager.GetInstance().RemoveMonster(parCollider.gameObject);
+
+            if(m_bounceCount < m_maxBounceCount)
+            {
+                FindOtherEnemy();
+            }
+        }
+
+    }
+
+    void FindOtherEnemy()
+    {
+        if(MonsterManager.GetInstance()._listOfMonster.Count > 0)
+        {
+            foreach(GameObject parMonster in MonsterManager.GetInstance()._listOfMonster)
+            {
+                float m_distance = Vector3.Distance(transform.position, parMonster.transform.position);
+                if(m_distance < m_currentDistanceMin)
+                {
+                    m_target = parMonster;
+                    m_currentDistanceMin = m_distance;
+                }
+            }
+
+            transform.DOMove(m_target.transform.position, 0.5f);
+            m_currentDistanceMin = Mathf.Infinity;
+        }
+    }
+}
