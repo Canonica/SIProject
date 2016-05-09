8804cabec37b8746b229659999131f996ee4ee38
Rodrigue Maurouard <r.maurouard@rubika-edu.com>
2016-05-09 18:26:29 +0200

    spawn monster debut

---
+++ Assets/Scripts/Monster/MonsterCage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterCage : MonoBehaviour {
+    public float _speed;
+    NavMeshAgent _agent;
+    public GameObject _target;
+	// Use this for initialization
+	void Start () {
+        _agent = this.GetComponent<NavMeshAgent>();
+        _target = GameObject.Find("Cage");
+        InvokeRepeating("FindTarget", 0.5f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    void FindTarget()
+    {
+        _agent.SetDestination(_target.transform.position);
+    }
+}
